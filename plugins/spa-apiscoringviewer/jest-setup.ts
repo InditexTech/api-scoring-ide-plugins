ef8d86ec5eee81d64a7b9c9c39983f309e141a13
Airam <airamrr@ext.inditex.com>
2023-07-05 12:44:14 +0200

    Removes commented line of code

--- plugins/spa-apiscoringviewer/jest-setup.ts
+++ plugins/spa-apiscoringviewer/jest-setup.ts
@@ -1,6 +1,3 @@
-// const originalGetComputedStyle = window.getComputedStyle;
-// window.getComputedStyle = (elt) => originalGetComputedStyle(elt);
-
 window.ResizeObserver = class ResizeObserver {
   observe() {}
   unobserve() {}
